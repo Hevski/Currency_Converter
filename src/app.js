b13372c5818739f97738eb308d671e437dde5856
Heather <heathercdavies@gmail.com>
2019-02-20 14:29:31 +0000

    can convert from any currency to any currency

--- src/app.js
+++ src/app.js
@@ -4,7 +4,32 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
-      
+      exchangeRates: {},
+      toCurrencyIndex: '',
+      toCurrency: null,
+      exchangeAmount: 0,
+      result: 0,
+      fromCurrency: 'EUR',
+      fromCurrencyIndex: ''
+    },
+    mounted(){
+      this.getRates()
+    },
+    methods: {
+      getRates: function (){
+        fetch('https://api.exchangeratesapi.io/latest')
+        .then(response => response.json())
+        .then(data => this.exchangeRates = data.rates)
+        .then(()=> this.exchangeRates['EUR'] = 1.0)
+      },
+      currencySelect: function (){
+        this.toCurrency = this.exchangeRates[this.toCurrencyIndex]
+      },
+      exchangeCurrency: function (){
+        const euros = this.exchangeAmount / this.exchangeRates[this.fromCurrencyIndex]
+        this.result = euros * this.exchangeRates[this.toCurrencyIndex]
+        this.result = Math.round(this.result * 100) / 100
+      }
     }
   })
 })
